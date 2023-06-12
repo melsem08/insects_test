b8f22853717d26a72927b91152adf139470285db
melsem08 <gersen18@gmail.com>
2023-06-12 12:31:02 +0100

    some commit

--- App.js
+++ App.js
@@ -1,10 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native";
+import { useState } from "react";
+import { ref, set } from "firebase/database";
+import { db } from "./components/config";
 
 export default function App() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  console.log(username);
+  console.log(email);
+  const handleClick = () => {
+    set(ref(db, "users/" + username), {
+      username: username,
+      email: email,
+    })
+      .then(() => {
+        setUsername("");
+        setEmail("");
+        alert("Successfully added");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textTop}>Test InsectiSpy</Text>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={(username) => {
+          setUsername(username);
+        }}
+        style={styles.textboxes}
+      ></TextInput>
+      <TextInput
+        placeholder="E-Mail"
+        value={email}
+        onChangeText={(email) => {
+          setEmail(email);
+        }}
+        style={styles.textboxes}
+      ></TextInput>
+      <button style={styles.submitButton} onClick={handleClick}>
+        Submit
+      </button>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +54,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textboxes: {
+    width: "90%",
+    fontSize: 18,
+    padding: 12,
+    borderColor: "gray",
+    borderWidth: 0.2,
+    borderRadius: 10,
+  },
+  textTop: {
+    fontSize: 30,
+    color: "green",
+  },
+  submitButton: {
+    margin: 15,
+    borderRadius: 15,
+    width: "25%",
+    height: "5%",
+    backgroundColor: "pink",
   },
 });
---
+++ components/config.jsx
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, onValue, get, child } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBc9HqIzNbtbPPlMOodyYoXCwLXcXAJYoU",
+  authDomain: "nc-insects-test.firebaseapp.com",
+  databaseURL:
+    "https://nc-insects-test-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "nc-insects-test",
+  storageBucket: "nc-insects-test.appspot.com",
+  messagingSenderId: "598833549168",
+  appId: "1:598833549168:web:8449e3afcd53c07c4149b5",
+  measurementId: "G-SBCZVXGKJK",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
